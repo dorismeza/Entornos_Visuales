07a484e40c3762131127813ca61605cb53f60c6e
admin <admin@DORISMEZA>
2020-05-28 21:27:46 -0600

    agregue 2 textbox que recogen el valor y muestra los resulyados y 2 botones que hace el calculo y el otro limpia

--- Ejercicio utilizando ciclo for/Form1.vb
+++ Ejercicio utilizando ciclo for/Form1.vb
@@ -1,3 +1,30 @@
 ﻿Public Class Form1
+    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+        Dim num As Integer
+        Dim i As Integer
 
+        txtResultado.Text = ""
+        If Not IsNumeric(txtNum.Text) Then
+
+            MsgBox("Ingrese un valor correcto")
+            txtNum.Text = ""
+        ElseIf txtNum.Text > 0 Then
+            num = Val(txtNum.Text)
+            For i = 1 To 10
+                txtResultado.Text = txtResultado.Text & num & " x " & i & " = " & (num * i) & vbCrLf
+            Next i
+        Else
+            MsgBox("Ingrese un valor correcto")
+            txtNum.Text = ""
+        End If
+
+
+
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+
+        txtResultado.Text = ""
+        txtNum.Text = ""
+    End Sub
 End Class
