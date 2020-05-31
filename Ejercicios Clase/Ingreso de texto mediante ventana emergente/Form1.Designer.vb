58cd1e758671173a5ab3f6e8be13330ffd18dccf
admin <admin@DORISMEZA>
2020-05-31 12:07:07 -0600

    Ingreso de texto mediante ventana emergente y ciclo for

---
+++ Ejercicios Clase/Ingreso de texto mediante ventana emergente/Form1.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.txtValor = New System.Windows.Forms.TextBox()
+        Me.cmbSuma = New System.Windows.Forms.ComboBox()
+        Me.cmbResta = New System.Windows.Forms.ComboBox()
+        Me.cmbMultiplicacion = New System.Windows.Forms.ComboBox()
+        Me.cmbDivision = New System.Windows.Forms.ComboBox()
+        Me.Suma = New System.Windows.Forms.Label()
+        Me.Resta = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtValor)
+        Me.GroupBox1.Controls.Add(Me.btnIngresar)
+        Me.GroupBox1.Location = New System.Drawing.Point(30, 25)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(304, 70)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingresar Valor"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnCalcular)
+        Me.GroupBox2.Controls.Add(Me.btnSalir)
+        Me.GroupBox2.Location = New System.Drawing.Point(30, 114)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(304, 64)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Opciones"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Resta)
+        Me.GroupBox3.Controls.Add(Me.Suma)
+        Me.GroupBox3.Controls.Add(Me.cmbDivision)
+        Me.GroupBox3.Controls.Add(Me.cmbMultiplicacion)
+        Me.GroupBox3.Controls.Add(Me.cmbResta)
+        Me.GroupBox3.Controls.Add(Me.cmbSuma)
+        Me.GroupBox3.Location = New System.Drawing.Point(30, 199)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(304, 210)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Resultado"
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.Location = New System.Drawing.Point(15, 28)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(80, 29)
+        Me.btnIngresar.TabIndex = 3
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(87, 28)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(62, 23)
+        Me.btnSalir.TabIndex = 4
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(6, 28)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 5
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'txtValor
+        '
+        Me.txtValor.Location = New System.Drawing.Point(108, 35)
+        Me.txtValor.Name = "txtValor"
+        Me.txtValor.Size = New System.Drawing.Size(100, 20)
+        Me.txtValor.TabIndex = 4
+        '
+        'cmbSuma
+        '
+        Me.cmbSuma.FormattingEnabled = True
+        Me.cmbSuma.Location = New System.Drawing.Point(15, 67)
+        Me.cmbSuma.Name = "cmbSuma"
+        Me.cmbSuma.Size = New System.Drawing.Size(121, 21)
+        Me.cmbSuma.TabIndex = 6
+        Me.cmbSuma.Text = "Mostrar suma"
+        '
+        'cmbResta
+        '
+        Me.cmbResta.FormattingEnabled = True
+        Me.cmbResta.Location = New System.Drawing.Point(160, 67)
+        Me.cmbResta.Name = "cmbResta"
+        Me.cmbResta.Size = New System.Drawing.Size(121, 21)
+        Me.cmbResta.TabIndex = 7
+        Me.cmbResta.Text = "Mostrar resta"
+        '
+        'cmbMultiplicacion
+        '
+        Me.cmbMultiplicacion.FormattingEnabled = True
+        Me.cmbMultiplicacion.Location = New System.Drawing.Point(15, 156)
+        Me.cmbMultiplicacion.Name = "cmbMultiplicacion"
+        Me.cmbMultiplicacion.Size = New System.Drawing.Size(121, 21)
+        Me.cmbMultiplicacion.TabIndex = 8
+        Me.cmbMultiplicacion.Text = "Mostrar multiplicacion"
+        '
+        'cmbDivision
+        '
+        Me.cmbDivision.FormattingEnabled = True
+        Me.cmbDivision.Location = New System.Drawing.Point(160, 156)
+        Me.cmbDivision.Name = "cmbDivision"
+        Me.cmbDivision.Size = New System.Drawing.Size(121, 21)
+        Me.cmbDivision.TabIndex = 9
+        Me.cmbDivision.Text = "Mostar division"
+        '
+        'Suma
+        '
+        Me.Suma.AutoSize = True
+        Me.Suma.Location = New System.Drawing.Point(15, 44)
+        Me.Suma.Name = "Suma"
+        Me.Suma.Size = New System.Drawing.Size(34, 13)
+        Me.Suma.TabIndex = 10
+        Me.Suma.Text = "Suma"
+        '
+        'Resta
+        '
+        Me.Resta.AutoSize = True
+        Me.Resta.Location = New System.Drawing.Point(164, 44)
+        Me.Resta.Name = "Resta"
+        Me.Resta.Size = New System.Drawing.Size(35, 13)
+        Me.Resta.TabIndex = 11
+        Me.Resta.Text = "Resta"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Multiplicacion"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(164, 132)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Division"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(376, 434)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtValor As TextBox
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Resta As Label
+    Friend WithEvents Suma As Label
+    Friend WithEvents cmbDivision As ComboBox
+    Friend WithEvents cmbMultiplicacion As ComboBox
+    Friend WithEvents cmbResta As ComboBox
+    Friend WithEvents cmbSuma As ComboBox
+End Class
---
+++ Ejercicios Clase/Ingreso de texto mediante ventana emergente/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        Dim valor As Integer
+        valor = InputBox("Ingrese un numero", "Ingreso")
+        txtValor.Text = valor
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Dim opcion As DialogResult
+        opcion = MessageBox.Show("¿Desea salir", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If opcion = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim suma, resta, multiplicacion, valor As Integer
+        Dim division As Double
+
+        valor = Val(txtValor.Text)
+        For i = 1 To 10 Step 1
+            suma = valor + i
+            resta = valor - i
+            multiplicacion = valor * i
+            division = Format((valor / i).ToString, 2)
+            cmbSuma.Items.Add(valor & " + " & i & "=" & suma)
+            cmbResta.Items.Add(valor & " - " & i & "=" & resta)
+            cmbMultiplicacion.Items.Add(valor & " x " & i & "=" & multiplicacion)
+            cmbDivision.Items.Add(valor & " / " & i & "=" & division)
+
+        Next
+
+    End Sub
+End Class
