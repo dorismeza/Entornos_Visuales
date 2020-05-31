f129ffd79c2b55555ef74275b2952cf6d450c5d2
admin <admin@DORISMEZA>
2020-05-31 10:30:06 -0600

    Factura

---
+++ Ejercicios Clase/Factura/Form1.Designer.vb
@@ -0,0 +1,258 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtDescu = New System.Windows.Forms.TextBox()
+        Me.txtSubt = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.chkDesc = New System.Windows.Forms.CheckBox()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.txtPunitario = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Enabled = False
+        Me.txtTotal.Location = New System.Drawing.Point(117, 151)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(116, 22)
+        Me.txtTotal.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(29, 152)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 16)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Total"
+        '
+        'txtDescu
+        '
+        Me.txtDescu.Enabled = False
+        Me.txtDescu.Location = New System.Drawing.Point(117, 106)
+        Me.txtDescu.Name = "txtDescu"
+        Me.txtDescu.Size = New System.Drawing.Size(116, 22)
+        Me.txtDescu.TabIndex = 5
+        '
+        'txtSubt
+        '
+        Me.txtSubt.Enabled = False
+        Me.txtSubt.Location = New System.Drawing.Point(117, 61)
+        Me.txtSubt.Name = "txtSubt"
+        Me.txtSubt.Size = New System.Drawing.Size(116, 22)
+        Me.txtSubt.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 65)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 16)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Subtotal"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox2.Controls.Add(Me.txtTotal)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.txtDescu)
+        Me.GroupBox2.Controls.Add(Me.txtSubt)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.GroupBox2.Location = New System.Drawing.Point(22, 236)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(259, 211)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Detalle de factura"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 110)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 16)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Descuento"
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnSalir.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnSalir.Location = New System.Drawing.Point(296, 145)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(87, 27)
+        Me.btnSalir.TabIndex = 8
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.BackColor = System.Drawing.Color.DarkGray
+        Me.btnNuevo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnNuevo.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnNuevo.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNuevo.ForeColor = System.Drawing.Color.Black
+        Me.btnNuevo.Location = New System.Drawing.Point(296, 95)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(87, 27)
+        Me.btnNuevo.TabIndex = 7
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = False
+        '
+        'chkDesc
+        '
+        Me.chkDesc.AutoSize = True
+        Me.chkDesc.BackColor = System.Drawing.Color.Transparent
+        Me.chkDesc.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.chkDesc.Location = New System.Drawing.Point(67, 142)
+        Me.chkDesc.Name = "chkDesc"
+        Me.chkDesc.Size = New System.Drawing.Size(137, 20)
+        Me.chkDesc.TabIndex = 6
+        Me.chkDesc.Text = "Aplicar Descuento"
+        Me.chkDesc.UseVisualStyleBackColor = False
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(113, 87)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(116, 22)
+        Me.txtCantidad.TabIndex = 5
+        '
+        'txtPunitario
+        '
+        Me.txtPunitario.Location = New System.Drawing.Point(113, 53)
+        Me.txtPunitario.Name = "txtPunitario"
+        Me.txtPunitario.Size = New System.Drawing.Size(116, 22)
+        Me.txtPunitario.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(24, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Cantidad"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(24, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "P. Unitario"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnCalcular.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalcular.ForeColor = System.Drawing.Color.Black
+        Me.btnCalcular.Location = New System.Drawing.Point(296, 44)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(87, 27)
+        Me.btnCalcular.TabIndex = 6
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GroupBox1.Controls.Add(Me.chkDesc)
+        Me.GroupBox1.Controls.Add(Me.txtCantidad)
+        Me.GroupBox1.Controls.Add(Me.txtPunitario)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.GroupBox1.Location = New System.Drawing.Point(22, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(259, 204)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos Requeridos"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(402, 472)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtDescu As TextBox
+    Friend WithEvents txtSubt As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents chkDesc As CheckBox
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents txtPunitario As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Ejercicios Clase/Factura/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim punitario, cantidad As Integer
+        Dim descuento, subtotal, total As Double
+        Dim aplidesc As Boolean
+
+        punitario = Val(txtPunitario.Text)
+        cantidad = Val(txtCantidad.Text)
+
+        aplidesc = chkDesc.Checked
+        descuento = 0
+        subtotal = punitario * cantidad
+
+        If aplidesc = True Then
+            descuento = subtotal * 0.05
+        End If
+
+        total = subtotal - descuento
+
+
+        'Valida textbox
+        If txtPunitario.Text = "" Then
+            MsgBox("Casilla vacía, ingrese un valor ", vbInformation)
+        ElseIf Not IsNumeric(txtPunitario.Text) Then
+            MsgBox("Ingrese un valor numerico", vbInformation)
+            txtPunitario.Text = ""
+        ElseIf txtPunitario.Text < 0 Then
+            MsgBox("Ingrese una cantidad correcta ", vbInformation)
+            txtPunitario.Text = ""
+        End If
+
+        If txtCantidad.Text = "" Then
+            MsgBox("Casilla vacía , ingrese un valor", vbInformation)
+        ElseIf Not IsNumeric(txtCantidad.Text) Then
+            MsgBox("Ingrese un valor numerico", vbInformation)
+            txtCantidad.Text = ""
+        ElseIf txtCantidad.Text < 0 Then
+            MsgBox("Ingrese una cantidad correcta ", vbInformation)
+            txtCantidad.Text = ""
+        End If
+
+
+        txtSubt.Text = subtotal
+        txtDescu.Text = descuento
+        txtTotal.Text = total
+
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+
+        txtCantidad.Clear()
+        txtDescu.Clear()
+        txtPunitario.Clear()
+        txtSubt.Clear()
+        txtTotal.Clear()
+
+    End Sub
+End Class
