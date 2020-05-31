b71a038240458f9d7cfb32ee15e410e1a76a6ecd
admin <admin@DORISMEZA>
2020-05-31 09:56:24 -0600

    Primeras validaciones y boton limpiar

---
+++ Ejercicios Clase/Primeras validaciones y botón limpiar/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.validar = New System.Windows.Forms.Button()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(139, 65)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 5
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'validar
+        '
+        Me.validar.Location = New System.Drawing.Point(32, 65)
+        Me.validar.Name = "validar"
+        Me.validar.Size = New System.Drawing.Size(75, 23)
+        Me.validar.TabIndex = 4
+        Me.validar.Text = "Validar"
+        Me.validar.UseVisualStyleBackColor = True
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(64, 26)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumero.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(252, 142)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.validar)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents validar As Button
+    Friend WithEvents txtNumero As TextBox
+End Class
---
+++ Ejercicios Clase/Primeras validaciones y botón limpiar/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub validar_Click(sender As Object, e As EventArgs) Handles validar.Click
+        Dim valor As Int16
+
+        If txtNumero.Text = "" Then
+            MsgBox("Escriba un numero en la casilla", vbInformation)
+        ElseIf Not IsNumeric(txtNumero.Text) Then
+            MsgBox("Ingrese un valor numerico", vbInformation)
+            txtNumero.Text = ""
+        ElseIf IsNumeric(valor) Then
+            valor = Val(txtNumero.Text)
+            If valor = 0 Then
+                MsgBox("Numero Cero", vbInformation)
+            ElseIf valor > 0 Then
+                MsgBox("Numero Positivo")
+            ElseIf valor < 0 Then
+                MsgBox("Numero Negativo")
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        txtNumero.Clear()
+    End Sub
+End Class
