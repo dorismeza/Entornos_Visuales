2493128499b0eb2459d4a853ff1ae1feb72da16a
admin <admin@DORISMEZA>
2020-06-03 00:32:33 -0600

    Entrega de Mascarillas

---
+++ Ejercicios Clase/Entrega_Mascarilla/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿Imports System.ComponentModel
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.btnEntregar = New System.Windows.Forms.Button()
+        Me.txtMunicipios = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtStock = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cmbDepartamento = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDepartamentos = New System.Windows.Forms.TextBox()
+        Me.btnSolicitar = New System.Windows.Forms.Button()
+        Me.ErrorProvider = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.btnEntregar)
+        Me.GroupBox3.Controls.Add(Me.txtMunicipios)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Location = New System.Drawing.Point(310, 29)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(277, 219)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Distribucion"
+        '
+        'btnEntregar
+        '
+        Me.btnEntregar.Location = New System.Drawing.Point(96, 92)
+        Me.btnEntregar.Name = "btnEntregar"
+        Me.btnEntregar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEntregar.TabIndex = 2
+        Me.btnEntregar.Text = "Entregar"
+        Me.btnEntregar.UseVisualStyleBackColor = True
+        '
+        'txtMunicipios
+        '
+        Me.txtMunicipios.Location = New System.Drawing.Point(131, 35)
+        Me.txtMunicipios.Name = "txtMunicipios"
+        Me.txtMunicipios.Size = New System.Drawing.Size(100, 20)
+        Me.txtMunicipios.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cant. Municipios"
+        '
+        'txtStock
+        '
+        Me.txtStock.Location = New System.Drawing.Point(496, 265)
+        Me.txtStock.Name = "txtStock"
+        Me.txtStock.Size = New System.Drawing.Size(90, 20)
+        Me.txtStock.TabIndex = 2
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.cmbDepartamento)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.txtDepartamentos)
+        Me.GroupBox2.Controls.Add(Me.btnSolicitar)
+        Me.GroupBox2.Location = New System.Drawing.Point(31, 29)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(273, 219)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Cantidad por departamento"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(135, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Cantidad por departamento"
+        '
+        'cmbDepartamento
+        '
+        Me.cmbDepartamento.FormattingEnabled = True
+        Me.cmbDepartamento.Location = New System.Drawing.Point(147, 145)
+        Me.cmbDepartamento.Name = "cmbDepartamento"
+        Me.cmbDepartamento.Size = New System.Drawing.Size(103, 21)
+        Me.cmbDepartamento.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Cant. Departamentos"
+        '
+        'txtDepartamentos
+        '
+        Me.txtDepartamentos.Location = New System.Drawing.Point(131, 51)
+        Me.txtDepartamentos.Name = "txtDepartamentos"
+        Me.txtDepartamentos.Size = New System.Drawing.Size(100, 20)
+        Me.txtDepartamentos.TabIndex = 4
+        '
+        'btnSolicitar
+        '
+        Me.btnSolicitar.Location = New System.Drawing.Point(144, 92)
+        Me.btnSolicitar.Name = "btnSolicitar"
+        Me.btnSolicitar.Size = New System.Drawing.Size(75, 23)
+        Me.btnSolicitar.TabIndex = 3
+        Me.btnSolicitar.Text = "Solicitar"
+        Me.btnSolicitar.UseVisualStyleBackColor = True
+        '
+        'ErrorProvider
+        '
+        Me.ErrorProvider.ContainerControl = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(618, 309)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.txtStock)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.ErrorProvider, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents btnEntregar As Button
+    Friend WithEvents txtMunicipios As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtStock As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cmbDepartamento As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtDepartamentos As TextBox
+    Friend WithEvents btnSolicitar As Button
+
+    Private Sub txtDepartamentos_TextChanged(sender As Object, e As EventArgs) Handles txtDepartamentos.TextChanged
+
+    End Sub
+
+    Private Sub txtDepartamentos_Validating(sender As Object, e As CancelEventArgs) Handles txtDepartamentos.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.ErrorProvider.SetError(sender, "")
+        Else
+            Me.ErrorProvider.SetError(sender, "Ingrese una cantidad")
+        End If
+    End Sub
+
+    Friend WithEvents ErrorProvider As ErrorProvider
+End Class
---
+++ Ejercicios Clase/Entrega_Mascarilla/Form1.vb
@@ -0,0 +1,425 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Private Sub btnSolicitar_Click(sender As Object, e As EventArgs) Handles btnSolicitar.Click
+        'For, DoWhile
+        Dim departamentos, mascarillas As Integer
+        departamentos = Val(txtDepartamentos.Text)
+
+        If Not IsNumeric(txtDepartamentos.Text) Then
+            MsgBox("Ingrese un valor numerico", vbInformation)
+            txtDepartamentos.Text = ""
+        End If
+
+        If departamentos > 0 And departamentos <= 18 Then
+            Try
+                For i = 1 To departamentos Step 1
+                    Do
+                        mascarillas = InputBox("Ingrese la cantidad de mascarillas " & i, ", Ingresar")
+                    Loop While (mascarillas < 0 Or mascarillas > 10000)
+                    cmbDepartamento.Items.Add(mascarillas)
+                Next
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+
+        Else
+            MsgBox("Numero de departamento incorrecto")
+            txtDepartamentos.Text = ""
+
+        End If
+
+
+    End Sub
+
+    Private Sub entregaMascarillas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbDepartamento.DropDownStyle = ComboBoxStyle.DropDownList
+    End Sub
+
+    Private Sub btnEntregar_Click(sender As Object, e As EventArgs) Handles btnEntregar.Click
+        Dim idDepartamento, municipios, cantEntrega As Integer
+        Dim itemDepartamento As Integer
+
+        idDepartamento = cmbDepartamento.SelectedIndex
+        itemDepartamento = cmbDepartamento.SelectedItem.ToString
+        municipios = txtMunicipios.Text
+
+
+        If Not IsNumeric(txtMunicipios.Text) Then
+            MsgBox("Ingrese un valor numerico", vbInformation)
+            txtMunicipios.Text = ""
+        End If
+
+
+        Casos(idDepartamento, municipios, cantEntrega, itemDepartamento)
+
+        txtStock.Text = itemDepartamento
+
+
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Public Function Casos(idD As Integer, muni As Integer, canEntrega As Integer, itemD As Integer)
+        Dim cont As Integer
+
+        Select Case idD
+            Case 0
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+            Case 1
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 2
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 3
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 4
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+            Case 5
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 6
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 7
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+            Case 8
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 9
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 10
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 11
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 12
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 13
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 14
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+            Case 15
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 16
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+            Case 17
+                Try
+                    For i = 1 To muni Step 1
+                        While (itemD > 0)
+                            canEntrega = InputBox("Ingrese la cantidad a entregar", "Entrega de mascarillas")
+                            If (itemD > canEntrega) Then
+                                muni += 1
+                                itemD -= canEntrega
+                                txtStock.Text = itemD
+                            Else
+                                MessageBox.Show("No hay suficientes mascarillas", "Error de entrega", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            End If
+                        End While
+                    Next
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
+        End Select
+
+        Return itemD
+    End Function
+
+    Private Sub txtMunicipios_TextChanged(sender As Object, e As EventArgs) Handles txtMunicipios.TextChanged
+
+    End Sub
+
+    Private Sub txtMunicipios_Validating(sender As Object, e As CancelEventArgs) Handles txtMunicipios.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.ErrorProvider.SetError(sender, "")
+        Else
+            Me.ErrorProvider.SetError(sender, "Ingrese una cantidad")
+        End If
+    End Sub
+End Class
