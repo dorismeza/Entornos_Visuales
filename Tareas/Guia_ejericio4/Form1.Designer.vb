9316dae8a0c04e8f24260b74e9f567d9e3335953
admin <admin@DORISMEZA>
2020-06-07 12:11:37 -0600

    Validacion de Numeros

---
+++ Tareas/Guia_ejericio4/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtNegativos = New System.Windows.Forms.TextBox()
+        Me.txtPares = New System.Windows.Forms.TextBox()
+        Me.txtImpares = New System.Windows.Forms.TextBox()
+        Me.txtPositivos = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.cmbNumIngresados = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtNegativos
+        '
+        Me.txtNegativos.Enabled = False
+        Me.txtNegativos.Location = New System.Drawing.Point(220, 207)
+        Me.txtNegativos.Name = "txtNegativos"
+        Me.txtNegativos.Size = New System.Drawing.Size(77, 20)
+        Me.txtNegativos.TabIndex = 0
+        '
+        'txtPares
+        '
+        Me.txtPares.Enabled = False
+        Me.txtPares.Location = New System.Drawing.Point(59, 163)
+        Me.txtPares.Name = "txtPares"
+        Me.txtPares.Size = New System.Drawing.Size(77, 20)
+        Me.txtPares.TabIndex = 1
+        '
+        'txtImpares
+        '
+        Me.txtImpares.Enabled = False
+        Me.txtImpares.Location = New System.Drawing.Point(59, 209)
+        Me.txtImpares.Name = "txtImpares"
+        Me.txtImpares.Size = New System.Drawing.Size(77, 20)
+        Me.txtImpares.TabIndex = 2
+        '
+        'txtPositivos
+        '
+        Me.txtPositivos.Enabled = False
+        Me.txtPositivos.Location = New System.Drawing.Point(220, 159)
+        Me.txtPositivos.Name = "txtPositivos"
+        Me.txtPositivos.Size = New System.Drawing.Size(77, 20)
+        Me.txtPositivos.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 166)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Pares"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 212)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Impares"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(165, 162)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Positivos"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(165, 210)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Negativos"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 251)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Suma Total"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Enabled = False
+        Me.txtTotal.Location = New System.Drawing.Point(79, 248)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(77, 20)
+        Me.txtTotal.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(43, 28)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(107, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Cantidad de numeros"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(46, 54)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(88, 20)
+        Me.txtCantidad.TabIndex = 10
+        '
+        'cmbNumIngresados
+        '
+        Me.cmbNumIngresados.FormattingEnabled = True
+        Me.cmbNumIngresados.Location = New System.Drawing.Point(170, 53)
+        Me.cmbNumIngresados.Name = "cmbNumIngresados"
+        Me.cmbNumIngresados.Size = New System.Drawing.Size(121, 21)
+        Me.cmbNumIngresados.TabIndex = 12
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(59, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(66, 23)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Validar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(177, 28)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(104, 13)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Numeros Ingresados"
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(195, 102)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(66, 23)
+        Me.btnNuevo.TabIndex = 16
+        Me.btnNuevo.Text = "Limpiar"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(336, 303)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cmbNumIngresados)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPositivos)
+        Me.Controls.Add(Me.txtImpares)
+        Me.Controls.Add(Me.txtPares)
+        Me.Controls.Add(Me.txtNegativos)
+        Me.Name = "Form1"
+        Me.Text = "Números"
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNegativos As TextBox
+    Friend WithEvents txtPares As TextBox
+    Friend WithEvents txtImpares As TextBox
+    Friend WithEvents txtPositivos As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents cmbNumIngresados As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents btnNuevo As Button
+End Class
---
+++ Tareas/Guia_ejericio4/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbNumIngresados.DropDownStyle = ComboBoxStyle.DropDownList
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim numero, cantidad, positivos, negativos, pares, impares, total As Integer
+
+        cantidad = Val(txtCantidad.Text)
+
+        Try
+            If Not IsNumeric(txtCantidad.Text) Then
+                MsgBox("Ingrese un numero")
+            End If
+            For i = 1 To cantidad Step 1
+                numero = InputBox("Ingrese numero " & i, "Ingreso de numeros")
+                cmbNumIngresados.Items.Add(numero)
+                total += numero
+                If numero > 0 Then
+                    positivos += 1
+                Else
+                    negativos += 1
+                End If
+                If numero Mod 2 = 0 Then
+                    pares += 1
+                Else
+                    impares += 1
+                End If
+            Next
+            txtPares.Text = pares
+            txtImpares.Text = impares
+            txtNegativos.Text = negativos
+            txtPositivos.Text = positivos
+            txtTotal.Text = total
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub txtCantidad_TextChanged(sender As Object, e As EventArgs) Handles txtCantidad.TextChanged
+
+    End Sub
+
+    Private Sub txtCantidad_Validating(sender As Object, e As CancelEventArgs) Handles txtCantidad.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.ErrorProvider1.SetError(sender, "")
+        Else
+            Me.ErrorProvider1.SetError(sender, "Ingrese una cantidad")
+        End If
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        Me.cmbNumIngresados.Items.Clear()
+        txtCantidad.Clear()
+        txtImpares.Clear()
+        txtPares.Clear()
+        txtNegativos.Clear()
+        txtPositivos.Clear()
+        txtTotal.Clear()
+
+    End Sub
+End Class
