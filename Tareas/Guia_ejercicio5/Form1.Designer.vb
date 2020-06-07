4b7cd9a7534aeca8cdfac3a0c5eb29dd589cd47c
admin <admin@DORISMEZA>
2020-06-07 15:00:06 -0600

    Calculo del Sueldo neto y dedecciobnes del SAR

---
+++ Tareas/Guia_ejercicio5/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtSalario = New System.Windows.Forms.TextBox()
+        Me.txtSueldoNeto = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDeduccion = New System.Windows.Forms.TextBox()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtSalario
+        '
+        Me.txtSalario.Location = New System.Drawing.Point(13, 37)
+        Me.txtSalario.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtSalario.Name = "txtSalario"
+        Me.txtSalario.Size = New System.Drawing.Size(125, 22)
+        Me.txtSalario.TabIndex = 0
+        '
+        'txtSueldoNeto
+        '
+        Me.txtSueldoNeto.Enabled = False
+        Me.txtSueldoNeto.Location = New System.Drawing.Point(94, 149)
+        Me.txtSueldoNeto.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtSueldoNeto.Name = "txtSueldoNeto"
+        Me.txtSueldoNeto.Size = New System.Drawing.Size(125, 22)
+        Me.txtSueldoNeto.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 17)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ingrese el salario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 152)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Sueldo Neto"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(182, 35)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 4
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(270, 35)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 5
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 107)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 16)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Deduccion"
+        '
+        'txtDeduccion
+        '
+        Me.txtDeduccion.Enabled = False
+        Me.txtDeduccion.Location = New System.Drawing.Point(91, 104)
+        Me.txtDeduccion.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtDeduccion.Name = "txtDeduccion"
+        Me.txtDeduccion.Size = New System.Drawing.Size(259, 22)
+        Me.txtDeduccion.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(388, 219)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtDeduccion)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSueldoNeto)
+        Me.Controls.Add(Me.txtSalario)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Deducciones SAR"
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSalario As TextBox
+    Friend WithEvents txtSueldoNeto As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtDeduccion As TextBox
+End Class
---
+++ Tareas/Guia_ejercicio5/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim salario, sueldo As Double
+        salario = Val(txtSalario.Text)
+        Try
+            If Not IsNumeric(txtSalario.Text) Then
+                MsgBox("Ingrese una cantidad correcta")
+            End If
+
+            If salario > 0.01 And salario <= 165482.06 Then
+                sueldo = salario
+                txtDeduccion.Text = "Excento"
+            ElseIf salario >= 165482.07 And salario <= 252330.81 Then
+                sueldo = salario - (salario * 0.15)
+                txtDeduccion.Text = "Impuesto del 15% = " & salario * 0.15
+            ElseIf salario >= 252330.82 And salario <= 586815.84 Then
+                sueldo = salario - (salario * 0.2)
+                txtDeduccion.Text = "Impuesto del 20% = " & salario * 0.2
+            ElseIf salario >= 586815.85 Then
+                sueldo = salario - (salario * 0.25)
+                txtDeduccion.Text = "Impuesto del 25% = " & salario * 0.25
+            End If
+            txtSueldoNeto.Text = sueldo
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtSalario.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_Validating(sender As Object, e As CancelEventArgs) Handles txtSalario.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.ErrorProvider1.SetError(sender, "")
+        Else
+            Me.ErrorProvider1.SetError(sender, "Ingrese una cantidad")
+        End If
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        txtSueldoNeto.Clear()
+        txtSalario.Clear()
+        txtDeduccion.Clear()
+    End Sub
+End Class
