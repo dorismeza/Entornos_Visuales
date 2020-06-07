9519b782640e8a278226b7e3f02485986f3a215f
admin <admin@DORISMEZA>
2020-06-07 10:08:22 -0600

    no terminado

--- Tareas/Guia_ejercicio3/Form1.Designer.vb
+++ Tareas/Guia_ejercicio3/Form1.Designer.vb
@@ -22,22 +22,30 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.cmbAlmacen = New System.Windows.Forms.ComboBox()
         Me.txtCantventas = New System.Windows.Forms.TextBox()
         Me.btnCalcular = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtlista = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.txtMedia = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'cmbAlmacen
         '
         Me.cmbAlmacen.FormattingEnabled = True
         Me.cmbAlmacen.Items.AddRange(New Object() {"Almacen SPS", "Almacen Tegucigalpa", "Almacen La Ceiba", "Almacen Olanchito", "Almacen Tocoa", "Almacen Juticalpa", "Almacen Siguatepeque", "Almacen El Progreso", "Almacen Villanueva", "Almacen Tela"})
-        Me.cmbAlmacen.Location = New System.Drawing.Point(12, 56)
+        Me.cmbAlmacen.Location = New System.Drawing.Point(4, 41)
         Me.cmbAlmacen.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.cmbAlmacen.Name = "cmbAlmacen"
         Me.cmbAlmacen.Size = New System.Drawing.Size(159, 26)
@@ -45,7 +53,7 @@ Partial Class Form1
         '
         'txtCantventas
         '
-        Me.txtCantventas.Location = New System.Drawing.Point(17, 156)
+        Me.txtCantventas.Location = New System.Drawing.Point(193, 40)
         Me.txtCantventas.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.txtCantventas.Name = "txtCantventas"
         Me.txtCantventas.Size = New System.Drawing.Size(132, 26)
@@ -53,7 +61,7 @@ Partial Class Form1
         '
         'btnCalcular
         '
-        Me.btnCalcular.Location = New System.Drawing.Point(217, 52)
+        Me.btnCalcular.Location = New System.Drawing.Point(255, 40)
         Me.btnCalcular.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.btnCalcular.Name = "btnCalcular"
         Me.btnCalcular.Size = New System.Drawing.Size(82, 32)
@@ -64,25 +72,26 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 34)
+        Me.Label1.Location = New System.Drawing.Point(3, 20)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(62, 18)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Almacén"
         '
-        'TextBox2
+        'txtlista
         '
-        Me.TextBox2.Location = New System.Drawing.Point(186, 120)
-        Me.TextBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(251, 229)
-        Me.TextBox2.TabIndex = 4
+        Me.txtlista.Enabled = False
+        Me.txtlista.Location = New System.Drawing.Point(42, 205)
+        Me.txtlista.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtlista.Multiline = True
+        Me.txtlista.Name = "txtlista"
+        Me.txtlista.Size = New System.Drawing.Size(380, 157)
+        Me.txtlista.TabIndex = 4
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(14, 134)
+        Me.Label2.Location = New System.Drawing.Point(190, 21)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(128, 18)
         Me.Label2.TabIndex = 5
@@ -91,7 +100,7 @@ Partial Class Form1
         'txtMedia
         '
         Me.txtMedia.Enabled = False
-        Me.txtMedia.Location = New System.Drawing.Point(15, 259)
+        Me.txtMedia.Location = New System.Drawing.Point(6, 48)
         Me.txtMedia.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.txtMedia.Name = "txtMedia"
         Me.txtMedia.Size = New System.Drawing.Size(132, 26)
@@ -100,7 +109,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(14, 237)
+        Me.Label3.Location = New System.Drawing.Point(6, 26)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(133, 18)
         Me.Label3.TabIndex = 7
@@ -108,7 +117,7 @@ Partial Class Form1
         '
         'btnNuevo
         '
-        Me.btnNuevo.Location = New System.Drawing.Point(329, 52)
+        Me.btnNuevo.Location = New System.Drawing.Point(343, 40)
         Me.btnNuevo.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.btnNuevo.Name = "btnNuevo"
         Me.btnNuevo.Size = New System.Drawing.Size(82, 32)
@@ -116,24 +125,64 @@ Partial Class Form1
         Me.btnNuevo.Text = "Nuevo"
         Me.btnNuevo.UseVisualStyleBackColor = True
         '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(343, 32)
+        Me.btnAgregar.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(82, 32)
+        Me.btnAgregar.TabIndex = 9
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cmbAlmacen)
+        Me.GroupBox1.Controls.Add(Me.btnAgregar)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtCantventas)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(17, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(457, 82)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnCalcular)
+        Me.GroupBox2.Controls.Add(Me.btnNuevo)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.txtMedia)
+        Me.GroupBox2.Location = New System.Drawing.Point(17, 100)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(457, 84)
+        Me.GroupBox2.TabIndex = 10
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(460, 375)
-        Me.Controls.Add(Me.btnNuevo)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.txtMedia)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.btnCalcular)
-        Me.Controls.Add(Me.txtCantventas)
-        Me.Controls.Add(Me.cmbAlmacen)
+        Me.ClientSize = New System.Drawing.Size(476, 375)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtlista)
         Me.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Form1"
         Me.Text = "Ozone S.A de C.V"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -143,9 +192,13 @@ Partial Class Form1
     Friend WithEvents txtCantventas As TextBox
     Friend WithEvents btnCalcular As Button
     Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents txtlista As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents txtMedia As TextBox
     Friend WithEvents Label3 As Label
     Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ErrorProvider1 As ErrorProvider
 End Class
--- Tareas/Guia_ejercicio3/Form1.vb
+++ Tareas/Guia_ejercicio3/Form1.vb
@@ -1,39 +1,110 @@
-﻿Public Class Form1
-    Dim media, mayor, acumulador As Double
-    Dim contador, idalmacen, itemalmacen, cantidad As Integer
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Dim media = 0,
+        acumulador,
+        cantidad, calcular As Double
+    Dim cant As Integer
+    Dim itemalmacen, idalmacen As String
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        txtCantventas.Clear()
+        txtlista.Clear()
+        txtMedia.Clear()
+        cmbAlmacen.Text = ""
+        media = 0
+        acumulador = 0
+    End Sub
+
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        idalmacen = cmbAlmacen.SelectedIndex
+        itemalmacen = cmbAlmacen.SelectedItem
+        cant = Val(txtCantventas.Text)
+
+        Try
+            If Not IsNumeric(txtCantventas.Text) Then
+                MsgBox("Ingrese un valor numerico", vbInformation)
+                txtCantventas.Text = ""
+            End If
+
+            If cmbAlmacen.Text = "" Then
+                MsgBox("Agregue un almacen")
+            Else
+                Select Case idalmacen
+                    Case 0
+                        Ingreso()
+
+                    Case 1
+                        Ingreso()
+
+                    Case 2
+                        Ingreso()
+
+                    Case 3
+                        Ingreso()
+
+                    Case 4
+                        Ingreso()
+
+                    Case 5
+                        Ingreso()
+                    Case 6
+                        Ingreso()
+
+                    Case 7
+                        Ingreso()
+
+                    Case 8
+                        Ingreso()
+
+                    Case 9
+                        Ingreso()
+
+                End Select
+
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Una empresa tiene 10 almacenes (cada 1 con su nombre correspondiente) 
         'y necesita crear un algoritmo que lea las ventas mensuales de cada uno de los mismos,
         'calcule la media de ventas mensual de todos los almacenes y obtenga un listado de los almacenes 
         'cuyas ventas mensuales son superiores a la media
+        cmbAlmacen.DropDownStyle = ComboBoxStyle.DropDownList
     End Sub
 
     Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
-        Dim media, mayor, acumulador, cantidad As Double
-        Dim contador, idalmacen, itemalmacen, cant As Integer
+        txtMedia.Text = media
+        If calcular > media Then
+            txtlista.Text = txtlista.Text & itemalmacen & " " & calcular & vbCrLf
+        End If
 
-        idalmacen = cmbAlmacen.SelectedIndex
-        itemalmacen = cmbAlmacen.SelectedItem.ToString
-        cant = Val(txtCantventas.Text)
-
+    End Sub
+    Private Sub Ingreso()
         Try
             For i = 1 To cant Step 1
-                cantidad = InputBox("Ingrese vental mensual" & i, & "")
-
-
+                cantidad = InputBox("Ingrese vental mensual " & i, "Ventas Mensuales")
+                acumulador += cantidad
             Next
+            media += (acumulador / 10)
+            calcular = cantidad
+
 
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
-
-        Select Case idalmacen
-            Case 0
-
-        End Select
-
     End Sub
-    Private Sub Ingreso()
 
+    Private Sub txtCantventas_Validating(sender As Object, e As CancelEventArgs) Handles txtCantventas.Validating
+        If DirectCast(sender, TextBox).Text.Length > 0 Then
+            Me.ErrorProvider1.SetError(sender, "")
+        Else
+            Me.ErrorProvider1.SetError(sender, "Ingrese una cantidad")
+        End If
     End Sub
 End Class
